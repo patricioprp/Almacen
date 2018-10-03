215c4499cc4daa61ca39169735d640b13d0eacc5
Patricio <patricioprp06@gmail.com>
2018-10-03 02:27:17 -0300

    se resuelven error en el main

--- resources/views/admin/template/main.blade.php
+++ resources/views/admin/template/main.blade.php
@@ -8,8 +8,7 @@
     
     <link rel="shortcut icon" href="{{ asset('images/favicon.png') }}">   
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
   </head>
   <body>
       @include('admin.template.partials.nav')
@@ -29,7 +28,10 @@
         </div>
     </section>
 
-
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
     <script src="{{ asset('plugins/chosen/chosen.jquery.js')}}"></script>
     {!! Html::script('js/dropdown.js')!!}
      @yield('js')
